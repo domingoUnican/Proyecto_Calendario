718baf710c033ab9d5ebbb00f43f971886a5a978
joservg13 <jrnotte13@gmail.com>
2017-02-10 08:53:46 +0100

    Update boton.py

--- boton.py
+++ boton.py
@@ -2,9 +2,11 @@ from kivy.uix.button import Button
 from horario import horario
 
 class Boton(Button):
+    select = 0
+    
     def on_press(self, *args):
         self.disabled = True
-        #self.printText()
+        self.select = 2
 
     def printText(self):
         print("El texto del boton es:"+ self.text)
