7ef507f074278a891c59048c6eca602de5dba880
joservg13 <jrnotte13@gmail.com>
2017-04-11 00:11:05 +0200

    pantalla completa
    Establezco la aplicación a pantalla completa según la resolución

--- main.py
+++ main.py
@@ -7,30 +7,54 @@ from horario import horario
 from Boxes import Boxes
 from kivy.uix.popup import Popup
 from kivy.uix.filechooser import FileChooserListView
+from kivy.config import Config
+from lxml import etree
 #from excel import excel
 
 #variables globales
-dias=['Lunes','Martes','Miércoles','Jueves','Viernes']
+dias=['Lunes','Martes','Miercoles','Jueves','Viernes']
 horarioPrincipal = horario(dias,timedelta(hours=9))
         
 class TestApp(App):
     def build(self):
+
+        #Modifico la configuracion de kivy
+        Config.set('graphics', 'fullscreen', 'auto')
+        Config.write()
+
+        #Leo el horario desde el archivo generado por KHE
+        #Inicializaciones
+        doc = etree.parse('outfile2.xml')
+        root = doc.getroot()
+
+        #Me posiciono en la solución generada
+        solutionGroup = doc.find('SolutionGroups')
+        solution = solutionGroup.find('SolutionGroup')
+        solution1 = solution.find('Solution')
+        events = solution1.find('Events')
+
+        for event in events:
+            reference = event.get('Reference')
+            print(reference)
+        
         horarioPrincipal.incluye_hora('Lunes','Mates',timedelta(hours=9), timedelta(hours=10))
         horarioPrincipal.incluye_hora('Lunes','Ingles',timedelta(hours=10), timedelta(hours=11))
         horarioPrincipal.incluye_hora('Martes','Lengua',timedelta(hours=9), timedelta(hours=11))
-        horarioPrincipal.incluye_hora('Miércoles','Frances',timedelta(hours=9), timedelta(hours=11))
-        horarioPrincipal.incluye_hora('Jueves','Frances',timedelta(hours=9), timedelta(hours=10))
+        horarioPrincipal.incluye_hora('Miercoles','Frances',timedelta(hours=9), timedelta(hours=11))
         horarioPrincipal.incluye_hora('Jueves','Ingles',timedelta(hours=10), timedelta(hours=11))
+        horarioPrincipal.incluye_hora('Jueves','Frances',timedelta(hours=9), timedelta(hours=10))
         horarioPrincipal.incluye_hora('Viernes','Frances',timedelta(hours=9), timedelta(hours=11))
 
-        for dia in ['Lunes','Martes','Miércoles','Jueves','Viernes']:
+        for dia in ['Lunes','Martes','Miercoles','Jueves','Viernes']:
             for i in range(11,19):
                 horarioPrincipal.incluye_hora(dia,'Libre',timedelta(hours=i), timedelta(hours=i+1))
 
         return Boxes(horarioPrincipal)
 
     def save(self):
+        print('PRINT MONEY')
         horarioPrincipal.save_timetableXML()
+        print('AFTER PRINT')
 
 if __name__ == '__main__':
     TestApp().run()
