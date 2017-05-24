292e2d38063a7b1843ee02d7aa57053319acd83b
joservg13 <jrnotte13@gmail.com>
2017-05-25 00:26:45 +0200

    Update boton.py

--- boton.py
+++ boton.py
@@ -4,6 +4,8 @@ from horario import horario
 class Boton(Button):
     select = 0
     ident = 0
+    asigID = ''
+    aulaID = ''
     
     def on_press(self, *args):
         self.disabled = True
@@ -14,6 +16,21 @@ class Boton(Button):
 
     def getIdent(self):
         return self.ident
+    
+    def setAsigID(self, asigID):
+        self.asigID = asigID
+
+    def getAsigID(self):
+        return self.asigID
+    
+    def setAulaID(self, aulaID):
+        self.aulaID = aulaID
+
+    def getAulaID(self):
+        return self.aulaID
 
     def printText(self):
         print("El texto del boton es:"+ self.text)
+
+    def setText(self, text):
+        self.text = text
