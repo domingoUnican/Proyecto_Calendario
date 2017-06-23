810b8c3e56d5a05cc82cb07faead8f67ec3b6960
mariomeissner <mr.rash.boy@gmail.com>
2017-06-23 20:11:52 +0200

    Initial push, WIP

--- helpers.py
+++ helpers.py
@@ -7,14 +7,14 @@ from variables import *
 
 
 def remove_accents(s):
-   return ''.join(c for c in unicodedata.normalize('NFD', s)
-                  if unicodedata.category(c) != 'Mn')
+    return ''.join(c for c in unicodedata.normalize('NFD', s)
+                   if unicodedata.category(c) != 'Mn')
 
 
 def size_room(x):
-    if 50<=x:
+    if 50 <= x:
         return 'Aula_Grande'
-    elif 25<x<50:
+    elif 25 < x < 50:
         return 'Aula_Normal'
     else:
         return 'Aula_Pequena'
@@ -22,38 +22,42 @@ def size_room(x):
 
 def hours_slot(i):
     j = int(i)
-    if j<= 2:
+    if j <= 2:
         return GRUPOS[0]
-    elif 2<= j <= 5:
+    elif 2 <= j <= 5:
         return GRUPOS[1]
     else:
         return GRUPOS[2]
 
 
 def time_class(s):
-   t = s.replace(',','.')
-   return float(t) if t else float(0)
+    t = s.replace(',', '.')
+    return float(t) if t else float(0)
+
 
 def normalize_name(s):
-   p = re.compile("\(([GC][0-9]+)\)|\(\*\)")
-   q = re.compile("\([GC][0-9]+\)")
-   if q.match(s):
-      return p.sub("", s).upper().strip(), q.match(s).group()[1:-1]
-   else:
-      return p.sub("", s).upper().strip(), ''
+    p = re.compile("\(([GC][0-9]+)\)|\(\*\)")
+    q = re.compile("\([GC][0-9]+\)")
+    if q.match(s):
+        return p.sub("", s).upper().strip(), q.match(s).group()[1:-1]
+    else:
+        return p.sub("", s).upper().strip(), ''
+
 
 def expected_number_students(array):
-   maximum = 0
-   for i in array:
-      num =i.strip()
-      if num:
-         maximum = max(int(num), maximum)
-   return maximum
+    maximum = 0
+    for i in array:
+        num = i.strip()
+        if num:
+            maximum = max(int(num), maximum)
+    return maximum
+
+
 def name_degree(l):
-   temp = l.split('_')
-   if len(temp)>1:
-      l0, year = temp
-   else:
-      l0, year = temp[0],''
-   s = normalize_name(l0)[0]
-   return s, year
+    temp = l.split('_')
+    if len(temp) > 1:
+        l0, year = temp
+    else:
+        l0, year = temp[0], ''
+    s = normalize_name(l0)[0]
+    return s, year
--- variables.py
+++ variables.py
@@ -2,58 +2,57 @@
 
 import os
 
-DIR = os.path.join('.','datos')
+DIR = os.path.join('.', 'datos')
 PROFS = [os.path.join(DIR, 'profesores_doblegrado.csv'),
          os.path.join(DIR, 'profesores_fisica.csv'),
          os.path.join(DIR, 'profesores_matematicas.csv'),
          os.path.join(DIR, 'profesores_informática.csv'),
-]
-ASIG = [os.path.join(DIR, 'asignaturas.csv'),
-]
+        ]
+SUBJ = os.path.join(DIR, 'asignaturas.csv')
 ROOMS = os.path.join(DIR, 'aulas.csv')
 OUT = os.path.join(DIR, 'outfile_nuevo.xml')
-GRUPOS = ['AntesDescanso', 'DespuesDescanso','Tarde']
-GRUPOS_NAMES = {
+GROUPS = ['AntesDescanso', 'DespuesDescanso', 'Tarde']
+GROUPS_NAMES = {
     'AntesDescanso': 'Antes del descanso',
-    'DespuesDescanso':'Despues del descanso',
-    'Tarde':'Tarde'
+    'DespuesDescanso': 'Despues del descanso',
+    'Tarde': 'Tarde'
 }
 DIAS = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes']
-HORAS = [str(i) for i in range(1,10)]
-RECURSOS = ['Teacher', 'Room', 'Class','Laboratory']
+HORAS = [str(i) for i in range(1, 10)]
+RECURSOS = ['Teacher', 'Room', 'Class', 'Laboratory']
 RECURSOS_NAME = {
     'Teacher': 'Profesores',
     'Room': 'Aulas',
     'Class': 'Cursos',
-    'Laboratory':'Laboratorio'
+    'Laboratory': 'Laboratorio'
 }
-DEGREES = ['DOBLE','FISICA','MATEMATICAS','INFORMATICA']
-TYPES_ROOMS = {'Aula_Grande':'Room',
-               'Aula_Normal':'Room',
-               'Laboratorio':'Laboratory',
-               'Aula_Pequena':'Room'}
+DEGREES = ['DOBLE', 'FISICA', 'MATEMATICAS', 'INFORMATICA']
+TYPES_ROOMS = {'Aula_Grande': 'Room',
+               'Aula_Normal': 'Room',
+               'Laboratorio': 'Laboratory',
+               'Aula_Pequena': 'Room'}
 TYPES_ROOMS_NAMES = {
-    'Aula_Grande':'Aula Grande',
-    'Aula_Normal':'Aula Normal',
-    'Laboratorio':'Laboratorio',
-    'Aula_Pequena':'Aula Pequena'
+    'Aula_Grande': 'Aula Grande',
+    'Aula_Normal': 'Aula Normal',
+    'Laboratorio': 'Laboratorio',
+    'Aula_Pequena': 'Aula Pequena'
 }
 CLASSES_NAME = dict()
 for degree in DEGREES:
-    for i in range(1,5):
-        key = 'Class_%s1_%s'%(str(i),degree )
-        value = 'Clase %s (grupo 1) de %s'%(str(i),degree)
+    for i in range(1, 5):
+        key = 'Class_%s1_%s' % (str(i), degree)
+        value = 'Clase %s (grupo 1) de %s' % (str(i), degree)
         CLASSES_NAME[key] = value
-        if i<=3:
-            key = 'Class_%s2_%s'%(str(i),degree )
-            value = 'Clase %s (grupo 2) de %s'%(str(i),degree)
+        if i <= 3:
+            key = 'Class_%s2_%s' % (str(i), degree)
+            value = 'Clase %s (grupo 2) de %s' % (str(i), degree)
             CLASSES_NAME[key] = value
 CLASSES = list(CLASSES_NAME.keys())
 CLASSES.sort()
-COURSE = dict(zip(PROFS,DEGREES))
+COURSE = dict(zip(PROFS, DEGREES))
 for asig in ASIG:
     for degree in DEGREES:
         if degree.lower() in asig:
             COURSE[asig] = degree
 
-N_GROUPS = {'1':4,'2':4, '3':1, '4':1}
+N_GROUPS = {'1': 4, '2': 4, '3': 1, '4': 1}
